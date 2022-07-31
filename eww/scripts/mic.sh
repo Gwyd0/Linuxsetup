83967d565316b5f9e76b3ab32d2513dad8aa6341
Gwydion <68437867+Gwyd0@users.noreply.github.com>
2022-07-31 19:45:26 +0100

    Delete mic.sh

--- eww/scripts/mic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if  amixer -D pulse sget Capture | grep 'off'; then
-  eww update mic-icon=""
-  notify-send "Mic" "You are now muted." -i "~/.config/dunst/img/mic.png"
-else
-  eww update mic-icon=""
-  notify-send "Mic" "You are now unmuted." -i "~/.config/dunst/img/mic.png"
-fi
